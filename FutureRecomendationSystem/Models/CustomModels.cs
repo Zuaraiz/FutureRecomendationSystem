9ceb77e20e96f487b7e6733f3b321bfa974b8e22
Muhammad-Usman17 <muhammadusman1794@gmail.com>
2018-02-20 02:31:02 +0500

    First Commit

---
+++ FutureRecomendationSystem/Models/CustomModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FutureRecomendationSystem.Models
+{
+    public class CustomModels:UserInfo
+    {
+        public String email { set; get; }
+        public String password { set; get; }
+
+    }
+}
