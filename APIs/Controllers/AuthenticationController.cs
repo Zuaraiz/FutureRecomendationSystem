c7ef745073878f75f71f0ff2db9f4f20fdacb421
Muhammad-Usman17 <muhammadusman1794@gmail.com>
2018-02-20 12:39:11 +0500

    Structure I

---
+++ APIs/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using APIs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIs.Controllers
+{
+    public class AuthenticationController : ApiController
+    {
+        [HttpPut]
+        [Route("api/Signup")]
+        public int SignUp([FromBody]CustomModels userData)
+        {
+            FRDBEntities db = new FRDBEntities();
+            int id = db.GetUserId(userData.email).FirstOrDefault<int?>() ?? default(int); ;
+            if (id.Equals(0))
+            {
+                return db.UserSignUp(userData.email, userData.password, userData.firstName, userData.lastName, userData.percentage, userData.annualBudget, userData.location, userData.qualification);
+
+            }
+            return 0;
+
+        }
+        [HttpPost]
+        [Route("api/Signin")]
+        public UserSignIn_Result SignIn([FromBody] User user)
+
+        {
+            FRDBEntities db = new FRDBEntities();
+            return db.UserSignIn(user.email, user.password).FirstOrDefault<UserSignIn_Result>();
+        }
+    }
+}
---
+++ APIs/Models/CustomModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIs.Models
+{
+    public class CustomModels:UserSignIn_Result
+    {
+        public String email { set; get; }
+        public String password { set; get; }
+
+    }
+}
